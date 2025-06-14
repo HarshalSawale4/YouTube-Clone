412cfff4c538d6701dca64945ef0d9ebdca8a2c4
Harshal V. Sawale <harshaltech6@gmail.com>
2025-06-14 12:53:36 +0530

    add new changes

--- js/welcome.js
+++ js/welcome.js
@@ -10,7 +10,7 @@ window.addEventListener('load', function () {
         
         // Redirect to the main page after the animation is complete
         setTimeout(function () {
-            window.location.href = 'page.html'; // Redirect to the main page
+            window.location.href = '../html_files/page.html'; // Redirect to the main page
         }, 2000); // Delay for 1 second to give time for the fade-out effect
     }, 3000); // Splash screen stays for 3 seconds
 });
